4cb3f9f12df3c6274ad3974628ba75bdf2446047
«nat» <nat27.kozlowa@yandex.ru>
2023-12-25 14:34:06 +0400

    семинар 4

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from check_post import get_login
+
+
+@pytest.fixture()
+def token():
+    return get_login()
+
+@pytest.fixture(scope="session")
+def browser():
+    if browser == 'firefox':
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
